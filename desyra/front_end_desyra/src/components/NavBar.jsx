8a09bbfbe2a4319b268f85b8a12b8354b6ebe356
rikko2123 <bigiriccardo2001@gmail.com>
2025-07-03 00:32:08 +0200

    creato nav mobile

--- desyra/front_end_desyra/src/components/NavBar.jsx
+++ desyra/front_end_desyra/src/components/NavBar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/logo_desyra.png'
 import '../style/navbar.css'
-
-const nav_element=[
-    {type: 'link', label: 'Shop', href: '/shop'},
-    {type: 'link', label: 'About us', href: '/about-us'},
-    {type: 'img',  src: Logo, href: '/'},
-    {type: 'link', label: 'Search', href: '/search'},
-    {type: 'link', label: 'Account', href: '/account'},
-    {type: 'link', label: 'Wishlist', href: '/wishlist'},
-    {type: 'link', label: 'Chart', href: '/chart'},
-
-]
+import MobileMenu from './MobileMenu'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
 function NavBar() {
+
+    const nav_element=[
+        {type: 'link', label: 'Shop', href: '/shop'},
+        {type: 'link', label: 'About us', href: '/about-us'},
+        {type: 'img',  src: Logo, href: '/'},
+        {type: 'link', label: 'Search', href: '/search', logo: <SearchIcon sx={{color:'black'}}/>},
+        {type: 'link', label: 'Account', href: '/account', },
+        {type: 'link', label: 'Wishlist', href: '/wishlist'},
+        {type: 'link', label: 'Chart', href: '/chart', logo: <ShoppingBagIcon sx={{color:'black'}}/>},
+    ]
+
     return (
         <nav style={{display:'flex', width:'100%', height:'4.5rem'}} className='nav-style'>
+
+            {/* menu mobile */}
+            <MobileMenu nav_elements = {nav_element}/>
+
+            {/* menu desktop */}
             <ul className='nav-style-left'>
                 {/* slice() è un metodo degli array JavaScript che serve per estrarre una porzione dell’array, senza modificarlo. */}
                 {nav_element?.slice(0, 2).map((nav_item, index) => (
@@ -44,6 +52,8 @@ function NavBar() {
                     </li> 
                 ))}                
             </ul>
+
+
         </nav>
     )
 }
